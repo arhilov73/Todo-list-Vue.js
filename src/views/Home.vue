6a38ae375b3e9ebcce339ead75a3b4ca83703f2a
arhilov73 <74810679+arhilov73@users.noreply.github.com>
2021-02-16 14:00:49 +0300

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tellus risus, dapibus ac odio nec, lacinia tempor urna. Proin non enim pharetra, aliquet libero non, dictum lectus. Integer eget lacus ac enim pharetra vulputate. Praesent nunc mi, congue porttitor tortor et, elementum ullamcorper diam. Aenean sit amet sapien in neque rutrum maximus sed ut erat. Nulla eget nulla diam. Fusce aliquam luctus sapien, a rutrum leo vestibulum nec. Pellentesque scelerisque, massa nec laoreet viverra, orci nulla faucibus odio, vel tincidunt massa ex at tortor. Quisque lacus magna, scelerisque accumsan dolor pharetra, tempus tincidunt velit. Maecenas fringilla enim vitae dolor vulputate, id volutpat nulla imperdiet.</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
